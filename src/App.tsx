465f9f5c996fdcb317cbccbf98fd6cfd93bc7eb2
Mist-Wolf <mazimbabwalya@gmail.com>
2025-12-02 18:13:06 +0200

    router basename

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/graceful-playgrounds">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
